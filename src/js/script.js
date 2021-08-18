c8fbfcaa31e01267214c884402d15810bd293e7f
Bondi14 <haysak_andrij@meta.ua>
2021-08-18 20:58:16 +0300

    Carousel added

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
-// let answer = confirm("Ви хочете залишитись на нашій сторінці?")
-// if (answer) {
-//     console.log("Дякуємо за ваш вибір")
-// }
-// else {
-//     console.log("до побачення!")
-// }
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+    });
+});
