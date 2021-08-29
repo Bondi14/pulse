a6e43800a7e90a5ce0e11e46c915c49267311847
Bondi14 <haysak_andrij@meta.ua>
2021-08-29 13:58:19 +0300

    page up button added

--- src/js/script.js
+++ src/js/script.js
@@ -91,13 +91,29 @@ $(document).ready(function(){
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
-            data: $(this).seralize()
+            data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
             $('form').trigger('reset');
         });
+        return false;
+    });
+
+    // smooth scroll
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
 });
